9fcf0306517d77fb1edf130cddd7c91694d96699
Zick Balázs <balazs.zick2@gmail.com>
2025-03-26 12:07:32 +0100

    backend: Dependency Injection

---
+++ Backend/Backend/Models/WateringBreakdownCell.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Models;
+
+public class WateringBreakdownCell
+{
+    public int Day { get; set; }
+    public double TotalConsumption { get; set; }
+    public string Type { get; set; }
+}
