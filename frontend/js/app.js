24fb24771a4bd11afd68f0836bc71876ada9ac19
Zick Balázs <balazs.zick2@gmail.com>
2025-03-26 12:05:40 +0100

    frontend: Form Submitting, Table Visualization improvements

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,136 +1,150 @@
-class Ficus{
-    Id;
-    #name;
-    #type;
-    #consumption;
-    #frequency;
-    constructor(){
-        this.Id = crypto.randomUUID();
-    }
+class Ficus {
+  Id;
+  #name;
+  #type;
+  #consumption;
+  #frequency;
+  constructor() {
+    this.Id = crypto.randomUUID();
+  }
 
-    GenerateFormInDom(node){
-        let inputGroup = document.createElement("div");
-        
-        inputGroup.classList.add("row");
-        inputGroup.appendChild(this.#makeInputNode("ficusName", "text", "Növény neve"));
-        inputGroup.appendChild(this.#makeInputNode("ficusType", "text", "Növény típusa"));
-        inputGroup.appendChild(this.#makeInputNode("ficusConsumption", "number", "Fogyasztása"));
-        inputGroup.appendChild(this.#makeInputNode("ficusFrequency", "number", "Gyakoriság"));
+  GenerateFormInDom(node) {
+    let inputGroup = document.createElement("div");
 
-        node.appendChild(inputGroup);
+    inputGroup.classList.add("row");
+    inputGroup.appendChild(
+      this.#makeInputNode("ficusName", "text", "Növény neve")
+    );
+    inputGroup.appendChild(
+      this.#makeInputNode("ficusType", "text", "Növény típusa")
+    );
+    inputGroup.appendChild(
+      this.#makeInputNode("ficusConsumption", "number", "Fogyasztása")
+    );
+    inputGroup.appendChild(
+      this.#makeInputNode("ficusFrequency", "number", "Gyakoriság")
+    );
 
-    }
+    node.appendChild(inputGroup);
+  }
 
-    GetObject(){
-        this.#getValuesFromNodes();
-        return {
-            "name": this.#name,
-            "type": this.#type,
-            "consumption": Number(this.#consumption),
-            "frequency": Number(this.#frequency)
-        }
-    }
+  GetObject() {
+    this.#getValuesFromNodes();
+    return {
+      name: this.#name,
+      type: this.#type,
+      consumption: Number(this.#consumption),
+      frequency: Number(this.#frequency),
+    };
+  }
 
+  #getValuesFromNodes() {
+    this.#name = document.querySelector(`#ficusName-${this.Id}`).value;
+    this.#type = document.querySelector(`#ficusType-${this.Id}`).value;
+    this.#consumption = document.querySelector(
+      `#ficusConsumption-${this.Id}`
+    ).value;
+    this.#frequency = document.querySelector(
+      `#ficusFrequency-${this.Id}`
+    ).value;
+  }
 
-    #getValuesFromNodes(){
-        this.#name = document.querySelector(`#ficusName-${this.Id}`).value;
-        this.#type = document.querySelector(`#ficusType-${this.Id}`).value;
-        this.#consumption = document.querySelector(`#ficusConsumption-${this.Id}`).value;
-        this.#frequency = document.querySelector(`#ficusFrequency-${this.Id}`).value;
-    }
+  #makeInputNode(name, type, labelText) {
+    let inputDiv = document.createElement("div");
+    let input = document.createElement("input");
+    let label = document.createElement("label");
 
-    #makeInputNode(name, type, labelText){
-        let inputDiv = document.createElement("div");
-        let input = document.createElement("input");
-        let label = document.createElement("label");
+    //inputs classes, other parameters
+    input.classList.add("form-control");
+    input.name = `${name}-${this.Id}`;
+    input.id = `${name}-${this.Id}`;
+    input.type = type;
+    input.classList.add("mb-3");
 
-        //inputs classes, other parameters
-        input.classList.add("form-control");
-        input.name = `${name}-${this.Id}`;
-        input.id = `${name}-${this.Id}`;
-        input.type = type;
-        input.classList.add("mb-3");
-        
-        //label classes, parameter
-        label.htmlFor = `${name}-${this.Id}`;
-        label.classList.add("form-label");
-        label.textContent = labelText;
+    //label classes, parameter
+    label.htmlFor = `${name}-${this.Id}`;
+    label.classList.add("form-label");
+    label.textContent = labelText;
 
-        //main div classes
-        inputDiv.classList.add("col-md-3");
+    //main div classes
+    inputDiv.classList.add("col-md-3");
 
-        //push label and input
-        inputDiv.appendChild(label);
-        inputDiv.appendChild(input);
+    //push label and input
+    inputDiv.appendChild(label);
+    inputDiv.appendChild(input);
 
-        return inputDiv;
-    }
+    return inputDiv;
+  }
 }
 
-class TableCell{
-    #day;
-    #consumption;
-    #type;    
+class TableCell {
+  #day;
+  #consumption;
+  #type;
 
-    constructor(day, consumption, type){
-        this.#day = day;
-        this.#consumption = consumption;
-        this.#type = type;
-    }
+  constructor(day, consumption, type) {
+    this.#day = day;
+    this.#consumption = consumption;
+    this.#type = type;
+  }
 
-    AddCellToDomElement(){
-        let x = document.createElement("td");
-        x.textContent = `${this.#consumption} l`;
-        x.classList.add(this.#type);
-        return x;
-    }
+  AddCellToDomElement() {
+    let x = document.createElement("td");
+    x.textContent = `${this.#consumption} l`;
+    x.classList.add(this.#type);
+    return x;
+  }
 }
 
 let Fici = [];
 let Table = [];
 
-let addRow = (week) =>{
-    let rowElement = document.createElement("tr");
-    
-    let weekIdentifierCell = document.createElement("td");
-    weekIdentifierCell.textContent = "";
-    rowElement.appendChild(weekIdentifierCell);
+let addRow = (week, weekNumber) => {
+  let rowElement = document.createElement("tr");
+  let weekIdentifierCell = document.createElement("td");
+  
+  weekIdentifierCell.textContent = `${weekNumber+1}.`;
+  rowElement.appendChild(weekIdentifierCell);
 
+  week.forEach((x) => {
+    rowElement.appendChild(x.AddCellToDomElement());
+  });
 
-    week.forEach(x=>{
-        rowElement.appendChild(x.AddCellToDomElement())
-    });
-
-    document.querySelector("#resultData").appendChild(rowElement);
-
-}
+  document.querySelector("#resultData").appendChild(rowElement);
+};
 
 let addFicus = () => {
-    let ficus = new Ficus();
-    Fici.push(ficus);
-    ficus.GenerateFormInDom(document.querySelector("#ficaeToAdd"))
-}
+  let ficus = new Ficus();
+  Fici.push(ficus);
+  ficus.GenerateFormInDom(document.querySelector("#ficaeToAdd"));
+};
 
 let makeTableForDOM = () => {
-    document.querySelector("#resultData").innerHTML = "";
-    Table.forEach(addRow);
-    document.querySelector("#result").classList.remove("d-none");
-}
+  document.querySelector("#resultData").innerHTML = "";
+  Table.forEach(addRow);
+  document.querySelector("#result").classList.remove("d-none");
+};
 
 let submitForm = () => {
-    let fici = Fici.map(e=>e.GetObject());
-    fetch('http://localhost:5069/Ficus', {
-        method: 'POST',
-        headers:{
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(fici),
-    }).then(data=>data.json()).then(response=>{
-        Table = response.map(e=>e.map(x=>new TableCell(x.day, x.totalConsumption, x.type)));
-        makeTableForDOM(document.querySelector("#result"))
-    }).catch(e=>{
-        console.error(e);
+  let fici = Fici.map((e) => e.GetObject());
+  fetch("http://localhost:5069/Ficus", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(fici),
+  })
+    .then((data) => data.json())
+    .then((response) => {
+      Table = response.map((e) =>
+        e.map((x) => new TableCell(x.day, x.totalConsumption, x.type))
+      );
+      makeTableForDOM(document.querySelector("#result"));
     })
-}
+    .catch((e) => {
+      console.error(e);
+    });
+};
 
-document.querySelector("#ficusAdder").addEventListener("click", addFicus)
+document.querySelector("#ficusAdder").addEventListener("click", addFicus);
+document.querySelector("#submit").addEventListener("click", submitForm);
